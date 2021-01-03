2d22ee6e3ed0f55384e0d29daa920a4bf3b88d95
jbloom <jbloom@fredhutch.org>
2021-01-02 18:14:18 -0800

    require newer `numpy`

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
     long_description=readme,
     license='GPLv3',
     install_requires=[
-        'numpy>=1.13',
+        'numpy>=1.17',
         'pandas>=0.23',
         'matplotlib>=3.0.0',
         'palettable',
