8c63e08a4d5280db4c5c4c2dcefafcfa53c1f739
jbloom <jbloom@fredhutch.org>
2023-10-11 09:54:26 -0700

    `black` format `./docs`

--- docs/conf.py
+++ docs/conf.py
@@ -21,14 +21,14 @@ import os
 import re
 import sys
 
-sys.path.insert(0, '{0}/../dmslogo'.format(os.path.abspath('.')))
+sys.path.insert(0, "{0}/../dmslogo".format(os.path.abspath(".")))
 
 
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-needs_sphinx = '1.0'
+needs_sphinx = "1.0"
 
 numfig = False
 
@@ -36,39 +36,39 @@ numfig = False
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-        'sphinx.ext.autodoc',
-        'sphinx.ext.doctest',
-        'sphinx.ext.mathjax',
-        'sphinx.ext.githubpages',
-        'sphinx.ext.viewcode',
-        'matplotlib.sphinxext.plot_directive',
-        'nbsphinx',
-        'nbsphinx_link',
-        ]
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.githubpages",
+    "sphinx.ext.viewcode",
+    "matplotlib.sphinxext.plot_directive",
+    "nbsphinx",
+    "nbsphinx_link",
+]
 
 # configuration to matplotlib.sphinxext.plot_directive
 plot_include_source = True
 plot_html_show_source_link = False
 # https://github.com/matplotlib/matplotlib/issues/4563#issuecomment-381366101
-plot_rcparams = {'savefig.bbox': 'tight'}
+plot_rcparams = {"savefig.bbox": "tight"}
 plot_apply_rcparams = True
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ".rst"
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
 # General information about the project.
-project = u'dmslogo'
+project = "dmslogo"
 copyright = f"2019--{datetime.datetime.now().year}, Bloom lab"  # noqa: A001
-author = u'the Bloom lab'
+author = "the Bloom lab"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -76,16 +76,16 @@ author = u'the Bloom lab'
 #
 # The short X.Y version.
 # Read in the version from file
-versionfile = os.path.abspath(os.path.join(
-        os.path.dirname(__file__), '../dmslogo/__init__.py'))
+versionfile = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "../dmslogo/__init__.py")
+)
 with open(versionfile) as f:
     versiontext = f.read()
-versionmatches = re.findall(
-            r'__version__\s+=\s+[\'"]([^\'"]+)[\'"]',
-            versiontext)
+versionmatches = re.findall(r'__version__\s+=\s+[\'"]([^\'"]+)[\'"]', versiontext)
 if len(versionmatches) != 1:
-    raise ValueError(f"found {len(versionmatches)} matches for version "
-                     f"in {versionfile}")
+    raise ValueError(
+        f"found {len(versionmatches)} matches for version " f"in {versionfile}"
+    )
 else:
     versionstring = versionmatches[0]
 # The full version, including alpha/beta/rc tags.
@@ -101,10 +101,10 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'README.rst']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "README.rst"]
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
@@ -115,28 +115,28 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
 html_theme_options = {
-        'logo': 'BloomLogo.jpg',
-        'logo_name': 'true',
-        'description': 'Sequence logos for deep mutational scanning',
-        'github_button': 'true',
-        'github_user': 'jbloomlab',
-        'github_repo': 'dmslogo',
-        'github_banner': 'true',
-        'travis_button': 'false',
-        'sidebar_width': '250px',
-        }
+    "logo": "BloomLogo.jpg",
+    "logo_name": "true",
+    "description": "Sequence logos for deep mutational scanning",
+    "github_button": "true",
+    "github_user": "jbloomlab",
+    "github_repo": "dmslogo",
+    "github_banner": "true",
+    "travis_button": "false",
+    "sidebar_width": "250px",
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -144,12 +144,12 @@ html_static_path = ['_static']
 # This is required for the alabaster theme
 # refs: http://alabaster.readthedocs.io/en/latest/installation.html#sidebars
 html_sidebars = {
-    '**': [
-        'about.html',
-        'navigation.html',
-        'relations.html',  # needs 'show_related': True theme option to display
-        'searchbox.html',
-        'donate.html',
+    "**": [
+        "about.html",
+        "navigation.html",
+        "relations.html",  # needs 'show_related': True theme option to display
+        "searchbox.html",
+        "donate.html",
     ]
 }
 
@@ -157,7 +157,7 @@ html_sidebars = {
 # -- Options for HTMLHelp output ------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'dmslogodoc'
+htmlhelp_basename = "dmslogodoc"
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -166,15 +166,12 @@ latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
     # 'papersize': 'letterpaper',
-
     # The font size ('10pt', '11pt' or '12pt').
     #
     # 'pointsize': '10pt',
-
     # Additional stuff for the LaTeX preamble.
     #
     # 'preamble': '',
-
     # Latex figure (float) alignment
     #
     # 'figure_align': 'htbp',
@@ -184,8 +181,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'dmslogo.tex', u'dmslogo Documentation',
-     u'the Bloom lab', 'manual'),
+    (master_doc, "dmslogo.tex", "dmslogo Documentation", "the Bloom lab", "manual"),
 ]
 
 
@@ -193,10 +189,7 @@ latex_documents = [
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [
-    (master_doc, 'dmslogo', u'dmslogo Documentation',
-     [author], 1)
-]
+man_pages = [(master_doc, "dmslogo", "dmslogo Documentation", [author], 1)]
 
 
 # -- Options for Texinfo output -------------------------------------------
@@ -205,7 +198,13 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'dmslogo', u'dmslogo Documentation',
-     author, 'dmslogo', 'Logo plots for deep mutational scanning data.',
-     'Miscellaneous'),
-    ]
+    (
+        master_doc,
+        "dmslogo",
+        "dmslogo Documentation",
+        author,
+        "dmslogo",
+        "Logo plots for deep mutational scanning data.",
+        "Miscellaneous",
+    ),
+]
